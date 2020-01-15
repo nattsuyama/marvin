41ba2b0fa2da33b8b041544e8f4b7a54c5da7c66
nattsuyama <59840484+nattsuyama@users.noreply.github.com>
2020-01-15 14:18:15 -0300

    F2.3 -  recebe uma string como argumento e retorna a mesma string com os três primeiros caracteres em maiúsculo.

--- fase02/ex03/maiusculo.js
+++ fase02/ex03/maiusculo.js
@@ -1,7 +1,7 @@
-function maiusculo(){
-    var n = "natalia";
-    var a = n.substring(0,3);
-    var b = n.substr(-4);
-    var c = a.toUpperCase();
-    console.log(c + b)
-}
+function maiusculo(natalia){
+var a = natalia;
+var b = a.substring(0,3)
+var c = b.toUpperCase()
+var d = a.substr(3)
+return (c+d)
+} 
