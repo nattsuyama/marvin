f18e2fbcc6a068c88989ffe98bba0d8e37c89b0a
nattsuyama <59840484+nattsuyama@users.noreply.github.com>
2020-01-15 14:26:48 -0300

    F2.4 -  F que recebe uma string como argumento e retorna apenas o primeiro caractere, em minúsculo.

--- fase02/ex04/minusculo.js
+++ fase02/ex04/minusculo.js
@@ -1,7 +1,6 @@
-function minusculo(){
-    var n = "natalia";
+function minusculo(natalia){
+    var n = natalia;
     var a = n.substring(0,1);
-    var b = n.substr(1);
-    var c = a.toUpperCase();
-    console.log(c + b)
-}
+    var b = a.toLowerCase();
+    return b
+} 
