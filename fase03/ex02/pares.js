b3e51b39097f016d88bc2a9e092dd513e4d9dc1f
nattsuyama <59840484+nattsuyama@users.noreply.github.com>
2020-01-16 18:19:20 -0300

    F3.2 - F que sempre recebe dois números como argumento Caso ambos os números sejam pares, deve retornar “Os números são pares”.

---
+++ fase03/ex02/pares.js
@@ -0,0 +1,7 @@
+function pares(n1,n2){
+var a = n1;
+var b = n2; 
+    if (n1 % 2 === 0 && n2 % 2 === 0)
+        return "Os números são pares";
+        else 
+        return "Os números não são pares"}
