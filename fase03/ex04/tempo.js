8ad196c03c67af383797990455d21314b54991e9
nattsuyama <59840484+nattsuyama@users.noreply.github.com>
2020-01-17 15:13:47 -0300

    F3.4- F que calcula a quantidade de tempo entre duas datas válidas. A unidade de medida ("dia", "mês" ou "ano") também será passada como argumento.

--- fase03/ex04/tempo.js
+++ fase03/ex04/tempo.js
@@ -1,18 +1,18 @@
 function tempo(data1, data2, unidade){
-   var a = new Date (data1);
-   var c = new Date (data2);
-   var b = unidade;
+ var a = new Date (data1);
+var c = new Date (data2);
+ var b = unidade;
  var f = Math.abs(a.getTime() - c.getTime()); 
  var g = Math.ceil(f / (1000 * 60 * 60 * 24));
- var x = Number(g);
+ var x = g.toFixed(0);
  var h = g/30;
- var y = Number (h);
+ var y = h.toFixed(0);
  var i = (g/30)/12;
- var z = Number (i);
+ var z = i. toFixed(0);
  if (b === "dia")
- return (0);
-    else if (b=== "mês")
-    return y.toFixed(0);
+ return  Number(x);
+else if (b === "mês")
+    return  Number(y);
     else if (b === "ano")
-    return z.toFixed(0);
+    return  Number(z)
 }
