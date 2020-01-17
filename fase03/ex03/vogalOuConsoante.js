af932515e9a1ada9de636c2fc340b6e9a596974c
nattsuyama <59840484+nattsuyama@users.noreply.github.com>
2020-01-17 13:45:46 -0300

    F3.3-função chamada “vogalOuConsoante”, que sempre recebe uma letra (maiúscula ou minúscula) como argumento Deve retornar “Vogal” caso a letra recebida seja uma vogal, ou “Consoante” caso seja uma consoante.

---
+++ fase03/ex03/vogalOuConsoante.js
@@ -0,0 +1,15 @@
+function vogalOuConsoante(letra){
+var a = letra
+if (a==="a" ||
+a === "e" ||
+a === "i" ||
+a=== "u" ||
+a === "o" ||
+a === "A" ||
+a === "E" ||
+a === "I" ||
+a === "O" ||
+a === "U") 
+return "Vogal"
+else return "Consoante"}
+
