141811e68f706ad0f14ce2d9b72b44707308008c
Natalia Tsuyama <59840484+nattsuyama@users.noreply.github.com>
2020-01-27 15:25:11 -0300

    F4.3-A função deve retornar o segundo múltiplo de 42 entre esses números (inclusos); Se não encontrar o número, deve retornar false e exibir “Não encontrado” no console.

--- fase04/ex03/por42.js
+++ fase04/ex03/por42.js
@@ -1,7 +1,10 @@
 function por42(n1, n2){
-   var a = (Math.floor(n1/42)+2)*42;
-   if (a <= n2 )
+   var a = (Math.floor(n1/42)+1)*42;
+    var b = (Math.floor(n1/42)+2)*42;
+   if (n1 % 42===0 && a<=n2 )
     return a;
+    if (n1 %42 !== 0 && b<= n2)
+    return b;
 else {console.log("Não encontrado");
 return false}
 }
