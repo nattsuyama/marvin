a54469eaf9f2708601f09851edc0cf20a4381798
Natalia Tsuyama <59840484+nattsuyama@users.noreply.github.com>
2020-02-10 14:47:21 -0300

    F6.2-função chamada “animais”, que sempre recebe uma array/lista de objetos/dicionário; A função deve remover a propriedade altura e adicionar a propriedade idade, começando do 5 e acrescentando um para cada objeto; Exemplo: a idade do primeiro animal é 5, a do segundo animal é 6 e assim por diante; Deve retornar a array/lista de objetos/dicionário, com as devidas alterações

--- fase06/ex02/animais.js
+++ fase06/ex02/animais.js
@@ -1,7 +1,14 @@
-function animais (x){
-for (i=0;i<x.length;i++) {
-    delete(x[i].altura)
-    x[i].idade=5+i
-}
-return x
-}
+function animais (array) {   
+    var  obj = array;
+    var novaIdade = 5;
+    var i = 0;
+    while (i< array.length) {
+    var posicao = obj[i];
+    delete posicao.altura;
+    if (posicao.idade !== null) {
+        posicao.idade = novaIdade}
+    novaIdade++;
+    i++;
+    }
+    return array}
+    
